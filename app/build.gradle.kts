47164fe444c545253236e5a96cc1b15490372f1c
Yehia003 <mohamedezio57@gmail.com>
2024-11-27 23:19:41 +0200

    F-try

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.project1"
         minSdk = 29
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -32,6 +33,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 }
 
